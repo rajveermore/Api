ea6375a6bbf65cfab1fef694ee4568207fcaac03
rajveermore <rajveermore07@gmail.com>
2023-09-04 10:45:13 +0530

    models and admin

--- Api/Apiapp/admin.py
+++ Api/Apiapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import EmployeeDetailsModel
+
+class EMployeeDetailsAdmin(admin.ModelAdmin):
+    list_display=['Emp_Id','Emp_FName','Emp_LName','Emp_Dob','Emp_Add','Emp_Gender']
+
+
+admin.site.register(EmployeeDetailsModel,EMployeeDetailsAdmin)
--- Api/Apiapp/models.py
+++ Api/Apiapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class EmployeeDetailsModel(models.Model):
+    Emp_Id=models.IntegerField(primary_key=True)
+    Emp_FName=models.CharField(max_length=30)
+    Emp_LName=models.CharField(max_length=30)
+    Emp_Dob=models.DateField()
+    Emp_Add=models.CharField(max_length=40)
+    GENDER_CHOICES = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+    )
+    Emp_Gender= models.CharField(max_length=1, choices=GENDER_CHOICES)
